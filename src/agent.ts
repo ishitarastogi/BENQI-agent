22bedb5da7ab9db90c02cc14e930c39e4cf1574e
ishita rastogi <46647968+ishitarastogi@users.noreply.github.com>
2022-03-19 02:03:55 +0530

    Update agent.ts

--- src/agent.ts
+++ src/agent.ts
@@ -12,13 +12,13 @@ import { BigNumber, utils, providers } from "ethers";
 import util from "./utils";
 
 const Qi_CONTRACT: string = "0x8729438EB15e2C8B576fCc6AeCdA6A148776C0F5";
-const AMOUNT_THRESHOLD: BigNumber = BigNumber.from(10 ** 6);
+const AMOUNT_THRESHOLD: BigNumber = BigNumber.from(10 ** 6); // 1M USD
 
 export const createFinding = (
   delegator: string,
   fromDelegate: string,
   toDelegate: string,
-  balance: any
+  balance: BigNumber
 ): Finding => {
   return Finding.fromObject({
     name: "Delegations Monitor",
@@ -36,12 +36,12 @@ export const createFinding = (
   });
 };
 
-export function provideHandleTransaction  (
+export function provideHandleTransaction(
   amountThreshold: BigNumber,
   QiToken: string,
 
   provider: providers.Provider
-): HandleTransaction  {
+): HandleTransaction {
   const BenQiContract = new ethers.Contract(
     QiToken,
     util.BALANCE_OF_FUNCTION,
@@ -59,7 +59,6 @@ export function provideHandleTransaction  (
     await Promise.all(
       delegateChangedEvent.map(async (event) => {
         const delegator = event.args.delegator;
-     
 
         const balanceOfDelegator = await BenQiContract.balanceOf(delegator, {
           blockTag: txEvent.blockNumber,
