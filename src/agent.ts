2506d49efe33d99dfd14c55cb6ea85f96fee9c9b
ishitarastogi <ishita3099@gmail.com>
2022-03-18 14:52:56 +0530

    Add agent file

--- src/agent.ts
+++ src/agent.ts
@@ -1,45 +1,88 @@
-import BigNumber from 'bignumber.js'
-import { 
-  BlockEvent, 
-  Finding, 
-  HandleBlock, 
-  HandleTransaction, 
-  TransactionEvent, 
-  FindingSeverity, 
-  FindingType 
-} from 'forta-agent'
+import {
+  Finding,
+  TransactionEvent,
+  FindingSeverity,
+  FindingType,
+  getEthersProvider,
+  ethers,
+} from "forta-agent";
+import { BigNumber, utils, providers } from "ethers";
 
-let findingsCount = 0
+import util from "./utils";
 
-const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
-  const findings: Finding[] = []
+const Qi_CONTRACT: string = "0x8729438EB15e2C8B576fCc6AeCdA6A148776C0F5";
+const AMOUNT_THRESHOLD: BigNumber = BigNumber.from(10 ** 6); // 1M USD
 
-  // limiting this agent to emit only 5 findings so that the alert feed is not spammed
-  if (findingsCount >= 5) return findings;
+const createFinding = (
+  delegator: string,
+  fromDelegate: string,
+  toDelegate: string,
+  balance: BigNumber
+): Finding => {
+  return Finding.fromObject({
+    name: "Delegations Monitor",
+    description: "Detect user with a huge balance delegating their votes",
+    alertId: "BENQI-2",
+    severity: FindingSeverity.Info,
+    type: FindingType.Info,
+    protocol: "BENQI",
+    metadata: {
+      delegator: delegator.toLowerCase(),
+      fromDelegate: fromDelegate.toLowerCase(),
+      toDelegate: toDelegate.toLowerCase(),
+      balance: balance.toString(),
+    },
+  });
+};
 
-  // create finding if gas used is higher than threshold
-  const gasUsed = new BigNumber(txEvent.gasUsed)
-  if (gasUsed.isGreaterThan("1000000")) {
-    findings.push(Finding.fromObject({
-      name: "High Gas Used",
-      description: `Gas Used: ${gasUsed}`,
-      alertId: "FORTA-1",
-      severity: FindingSeverity.Medium,
-      type: FindingType.Suspicious
-    }))
-    findingsCount++
-  }
+export function provideHandleTransaction(
+  amountThreshold: BigNumber,
+  QiToken: string,
 
-  return findings
+  provider: providers.Provider
+) {
+  const BenQiContract = new ethers.Contract(
+    QiToken,
+    util.BALANCE_OF_FUNCTION,
+    provider
+  );
+  return async (txEvent: TransactionEvent) => {
+    const findings: Finding[] = [];
+    const delegateChangedEvent = txEvent.filterLog(
+      util.DELEGATE_CHANGED_EVENT,
+      QiToken
+    );
+
+    if (delegateChangedEvent.length === 0) return findings;
+
+    await Promise.all(
+      delegateChangedEvent.map(async (event) => {
+        const delegator = event.args.delegator;
+     
+
+        const balanceOfDelegator = await BenQiContract.balanceOf(delegator, {
+          blockTag: txEvent.blockNumber,
+        });
+        if (balanceOfDelegator.gte(amountThreshold)) {
+          const newFinding: Finding = createFinding(
+            event.args.delegator,
+            event.args.fromDelegate,
+            event.args.toDelegate,
+            balanceOfDelegator
+          );
+          findings.push(newFinding);
+        }
+      })
+    );
+
+    return findings;
+  };
 }
-
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
 
 export default {
-  handleTransaction,
-  // handleBlock
-}
+  handleTransaction: provideHandleTransaction(
+    AMOUNT_THRESHOLD,
+    Qi_CONTRACT,
+    getEthersProvider()
+  ),
+};
